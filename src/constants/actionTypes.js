6c3ee9b80463d6c73fefa194741c9ed1ce4935fa
tomas.komprs <tomas.komprs@studentagency.cz>
2020-01-16 15:20:51 +0100

    Refactoring ActionTypes

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,8 +1,4 @@
 export const GET_PEOPLE_DATA = 'GET_PEOPLE_DATA'
-export const GET_DATA_INFO = 'GET_DATA_INFO'
-
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 
 export const MAKE_ORDER = 'MAKE_ORDER'
 
